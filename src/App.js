03f61de3991fd7d49a5b7f628b9acd8b5b515966
JeffersonAguiar <jfdruida@gmail.com>
2024-10-03 22:27:58 +0100

    chore: Update landing page with new header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1>How Are You Really</h1>
-      <p>A discreet platform for sharing wellness stories.</p>
+      <Header />
+      <main>
+        <h2>Welcome to How Are You Really</h2>
+        <p>A discreet platform for sharing wellness stories.</p>
+      </main>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <h1>How Are You Really</h1>
+        <div>
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

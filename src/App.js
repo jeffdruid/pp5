cb96ef7de39db8a7683d241e5c885f763a3613f8
JeffersonAguiar <jfdruida@gmail.com>
2024-10-03 22:22:20 +0100

    chore: Refactor App component and update landing page content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React App</h1>
-        <h2> PP5 - Code Institute Final Project</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>How Are You Really</h1>
+      <p>A discreet platform for sharing wellness stories.</p>
     </div>
   );
 }

491112aac81c82d58529f06ef4fcfa2471a38218
JeffersonAguiar <jfdruida@gmail.com>
2024-10-08 17:35:59 +0100

    Refactor PostFeed.js to remove unused code and pass handleEditPost function to Post component

--- src/components/PostFeed.js
+++ src/components/PostFeed.js
@@ -1,48 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Post from './Post';
 import { Container } from 'react-bootstrap';
 
-function PostFeed() {
-  // Move posts data into state
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      author: 'Alex',
-      content: "I've been feeling a bit overwhelmed lately.",
-      mood: '😟',
-      created_at: new Date(),
-      likes: 5,
-      is_anonymous: true,
-    },
-    {
-      id: 2,
-      author: 'Alex',
-      content: 'Today was a good day! I achieved my goals.',
-      mood: '😊',
-      created_at: new Date(),
-      likes: 10,
-      is_anonymous: false,
-    },
-    {
-      id: 3,
-      author: 'Jane',
-      content: 'Feeling grateful for the little things in life.',
-      mood: '😊',
-      created_at: new Date(),
-      likes: 7,
-      is_anonymous: false,
-    },
-    {
-      id: 4,
-      author: 'John',
-      content: 'Feeling lost...',
-      mood: '😢',
-      created_at: new Date(),
-      likes: 3,
-      is_anonymous: true,
-    },
-  ]);
-
+function PostFeed({ posts, setPosts, handleEditPost }) {
   // Function to delete a post by id
   const deletePost = (id) => {
     // Filter out the post with the given id
@@ -54,8 +14,13 @@ function PostFeed() {
     <Container className="my-4">
       {posts.length > 0 ? (
         posts.map((post) => (
-          // Pass the deletePost function to each Post component
-          <Post key={post.id} post={post} deletePost={deletePost} />
+          // Pass the deletePost and handleEditPost functions to each Post component
+          <Post
+            key={post.id}
+            post={post}
+            deletePost={deletePost}
+            handleEditPost={handleEditPost}
+          />
         ))
       ) : (
         <p className="text-center">No posts available.</p>

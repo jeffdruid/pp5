10c7de0213f00593ed3e5d5e88d85d6660fb39b0
JeffersonAguiar <jfdruida@gmail.com>
2024-10-16 15:58:09 +0100

    Refactor: Update axiosDefaults.js to import axios with double quotes

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 axios.defaults.baseURL = 'https://drf-api-jeff-00b8a22f06d7.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';

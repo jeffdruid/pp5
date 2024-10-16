d45f33fd5e4e26a856d248b8101a3c287a90c875
JeffersonAguiar <jfdruida@gmail.com>
2024-10-16 11:33:15 +0100

    Refactor: Update SignInForm component for user authentication

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,5 +1,6 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
@@ -7,55 +8,42 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
+
 import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import { SetCurrentUserContext } from "../../App";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function SignInForm() {
-  const setCurrentUser = useContext(SetCurrentUserContext);
+  const setCurrentUser = useSetCurrentUser();
 
-  // State for form data
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
   });
   const { username, password } = signInData;
 
-  // State to handle errors
   const [errors, setErrors] = useState({});
 
-  // History hook to navigate after successful login
   const history = useHistory();
-
-  // Handle form submission
   const handleSubmit = async (event) => {
-  event.preventDefault();
-  try {
-    // Clear any previous errors
-    setErrors({});
+    event.preventDefault();
 
-    // Make POST request to login endpoint
-    const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+       // Store access and refresh tokens in localStorage
+      localStorage.setItem('access_token', data.access_token);  // Replace 'access_token' with the actual key from the login response
+      localStorage.setItem('refresh_token', data.refresh_token);  // Store refresh token
 
-    // Store access token in localStorage or cookies (depending on your setup)
-    localStorage.setItem('access_token', data.access_token);
+      setCurrentUser(data.user);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
-    // Set the current user in context
-    setCurrentUser(data.user);
-
-    // Redirect to the homepage after successful login
-    history.push("/");
-  } catch (err) {
-    // Set errors returned from the API to the errors state
-    setErrors(err.response?.data || {});
-  }
-};
-
-
-  // Handle form input changes
   const handleChange = (event) => {
     setSignInData({
       ...signInData,
@@ -66,11 +54,9 @@ function SignInForm() {
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4`}>
-          <h1 className={styles.Header}>Sign In</h1>
-          
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign in</h1>
           <Form onSubmit={handleSubmit}>
-            {/* Username Input */}
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
@@ -82,14 +68,12 @@ function SignInForm() {
                 onChange={handleChange}
               />
             </Form.Group>
-            {/* Display Username Errors */}
             {errors.username?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
 
-            {/* Password Input */}
             <Form.Group controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
@@ -101,22 +85,17 @@ function SignInForm() {
                 onChange={handleChange}
               />
             </Form.Group>
-            {/* Display Password Errors */}
             {errors.password?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
-
-            {/* Submit Button */}
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
               type="submit"
             >
-              Sign In
+              Sign in
             </Button>
-
-            {/* Display Non-field Errors (e.g., incorrect credentials) */}
             {errors.non_field_errors?.map((message, idx) => (
               <Alert key={idx} variant="warning" className="mt-3">
                 {message}
@@ -124,14 +103,12 @@ function SignInForm() {
             ))}
           </Form>
         </Container>
-
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
             Don't have an account? <span>Sign up now!</span>
           </Link>
         </Container>
       </Col>
-
       <Col
         md={6}
         className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
